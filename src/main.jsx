04ac8731873a4411847fa994c9aff1948e590022
TasneemWorld <tasneemmatsust2017115@gmail.com>
2023-04-29 09:50:00 +0600

    landering login logout

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import route from './routes/Routes.jsx';
 import { RouterProvider, Routes } from 'react-router-dom';
+import AuthProvider from './Provider/AuthProvider.jsx';
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={route}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={route}></RouterProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
 import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Provider/AuthProvider';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
+
+    // const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
+
     return (
         <Container>
             <div className='text-center'>
@@ -19,21 +25,22 @@ const Header = () => {
                     I can be a React component, multiple React components, or just some text.
                 </Marquee>
             </div>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className='mb-3'>
                 <Container>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mx-auto">
-                            <Nav.Link href="#home">
-                                <Link to='/'>Home</Link>
-                            </Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="#career">Career</Nav.Link>
+                        <Nav className="mx-auto d-flex">
+                            <Link className='text-decoration-none mx-2' to='/'>Home</Link>
+                            <Link className='text-decoration-none mx-2' to='/career'>Career</Link>
+                            <Link className='text-decoration-none mx-2' to='/best-news'>Best News</Link>
                         </Nav>
                         <Nav className='d-flex justify-content-center align-items-center'>
-                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            {user && <Nav.Link href="#deets">
+                                <FaUserCircle style={{fontSize: '2rem'}}></FaUserCircle>
+                            </Nav.Link>}
                             <Nav.Link eventKey={2} href="#memes">
-                                <Button variant="dark">Dark</Button>
+                               {user ? <Button variant="dark">LogOut</Button> :
+                                <Button variant="dark">LogIn</Button>}
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
